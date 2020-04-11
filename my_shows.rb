50cfa4ad30add26bd0f8a3bde4996fd47268d440
moffire <alexeydudarev.spb@gmail.com>
2020-04-11 18:15:01 +0300

    added parser for main page

--- my_shows.rb
+++ my_shows.rb
@@ -12,4 +12,20 @@ class MyShows
     Nokogiri::HTML(open(self.url))
   end
 
+  def top_rated(html)
+    movies = {}
+    series_list = html.xpath('/html/body/div[1]/div[4]/div/div')[0..1].css('a')
+    series_list.each do |movie|
+      ru_title = movie.css('.fsHeader').text
+      movies[:"#{ru_title}"] = {
+          en_title: movie.css('.cFadeLight').text,
+          image_link: movie.search('._img').to_s[/(?<=\().+?(?=\))/],
+          kp_link: movie['href']
+      } end
+    movies
+  end
 end
+
+ms = MyShows.new('https://myshows.me/')
+main_html = ms.main_page
+puts ms.top_rated(main_html)

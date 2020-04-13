088ba5cd1d308780f4049c3dd6ba8fd522b22a7d
moffire <alexeydudarev.spb@gmail.com>
2020-04-13 20:12:13 +0300

    changed all methods

--- my_shows.rb
+++ my_shows.rb
@@ -4,18 +4,17 @@ require 'cgi/util'
 
 class MyShows
 
-  def get_html(query = nil)
-    main_page_url = 'https://myshows.me'
-    if query.nil?
-      Nokogiri::HTML(open(main_page_url))
-    else
-      encoded_query = CGI.escape(query)
-      Nokogiri::HTML(open(main_page_url + "/search/?q=#{encoded_query}"))
-    end
+  MAIN_PAGE_URL = 'https://myshows.me'
+
+  attr_accessor :query
+
+  def initialize(query = nil)
+    @query = query
   end
 
-  # parse list of top rated movies from main page
-  def top_rated(html)
+  # top rated movies from main page
+  def top_rated
+    html = Nokogiri::HTML(URI.open(MAIN_PAGE_URL))
     top_rated_list = {}
     movies_list = html.xpath('/html/body/div[1]/div[4]/div/div')[0..1].css('a')
     movies_list.each do |movie|
@@ -29,24 +28,30 @@ class MyShows
     top_rated_list
   end
 
-  # parse list of searched series
-  def series_list(html)
-    series = {}
-    all_series = html.css('table.catalogTable').search('tr')
-    all_series[1...-1].each do |movie|
-      ru_title = movie.css('td > a')[0].text
-      series[:"#{ru_title}"] = {
-        en_title: movie.css('td > .catalogTableSubHeader')[0].text,
-        link: movie.css('td > a')[0]['href'],
-        watchers: movie.css('td')[2].text,
-        seasons: movie.css('td')[4].text,
-        year: movie.css('td')[5].text
-      }
+  # hash of searched series or top rated movies if no query
+  def movies_list
+    if @query.nil?
+      top_rated
+    else
+      content = {}
+      encoded_query = CGI.escape(@query)
+      html = Nokogiri::HTML(URI.open(MAIN_PAGE_URL + "/search/?q=#{encoded_query}"))
+      all_series = html.css('table.catalogTable').search('tr')
+      all_series[1...-1].each do |movie|
+        ru_title = movie.css('td > a')[0].text
+        content[:"#{ru_title}"] = {
+          en_title: movie.css('td > .catalogTableSubHeader')[0].text,
+          link: movie.css('td > a')[0]['href'],
+          watchers: movie.css('td')[2].text,
+          seasons: movie.css('td')[4].text,
+          year: movie.css('td')[5].text
+        }
+      end
+      content
     end
-    series
   end
 
 end
 
 ms = MyShows.new
-puts ms.series_list(ms.get_html('главный'))
+puts ms.movies_list.keys

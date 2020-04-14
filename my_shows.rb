33a2ecab80b7061d600fb086b392f7cd068b62d2
moffire <alexeydudarev.spb@gmail.com>
2020-04-14 22:34:50 +0300

    added logger and  exceptions

--- my_shows.rb
+++ my_shows.rb
@@ -1,10 +1,27 @@
+require 'logger'
 require 'nokogiri'
 require 'open-uri'
 require 'cgi/util'
 
-class MyShows
+module Logging
+  class << self
 
-  BASE_URL = 'https://myshows.me'
+    def logger
+      @logger ||= Logger.new(STDOUT)
+    end
+
+  end
+
+  def logger
+    Logging.logger
+  end
+
+end
+
+class MyShows
+  include Logging
+
+  BASE_URL = 'https://myshowsqq.me'.freeze
 
   attr_accessor :query
 
@@ -12,51 +29,41 @@ class MyShows
     @query = query
   end
 
-  # top rated movies from main page
-  def top_rated
-    html = Nokogiri::HTML(URI.open(BASE_URL))
-    top_rated_list = {}
-    # list of all movies
-    html.xpath('/html/body/div[1]/div[4]/div/div')[0..1].css('a').each do |movie|
-      ru_title = movie.css('.fsHeader').text
-      top_rated_list[:"#{ru_title}"] = {
-        en_title: movie.css('.cFadeLight').text,
-        image_link: movie.search('._img').to_s[/(?<=\().+?(?=\))/],
-        description: movie['href']
-      }
-    end
-    top_rated_list
+  def parse_html(url = '')
+    Nokogiri::HTML(URI.open(BASE_URL + url))
+  rescue StandardError
+    logger.error 'Internal error'
+    nil
   end
 
   # hash of searched series or top rated movies if no query
   def movies_list
-    if @query.nil?
-      top_rated
-    else
-      content = {}
-      encoded_query = CGI.escape(@query)
-      html = Nokogiri::HTML(URI.open(BASE_URL + "/search/?q=#{encoded_query}"))
-      all_series = html.css('table.catalogTable').search('tr')
-      all_series[1...-1].each do |movie|
-        ru_title = movie.css('td > a')[0].text
-        content[:"#{ru_title}"] = {
-          en_title: movie.css('td > .catalogTableSubHeader')[0].text,
-          link: movie.css('td > a')[0]['href'],
-          movie_id: movie.css('td > a')[0]['href'][/\d+/],
-          watchers: movie.css('td')[2].text,
-          seasons: movie.css('td')[4].text,
-          year: movie.css('td')[5].text
-        }
-      end
-      content
+    return top_rated if query.nil?
+
+    content = {}
+    encoded_query = CGI.escape(@query)
+    html = parse_html("/search/?q=#{encoded_query}")
+    all_series = html&.css('table.catalogTable').search('tr')
+    all_series[1...-1]&.each do |movie|
+      ru_title = movie.css('td > a')[0].text
+      content[:"#{ru_title}"] = {
+        en_title: movie.css('td > .catalogTableSubHeader')[0].text,
+        link: movie.css('td > a')[0]['href'],
+        movie_id: movie.css('td > a')[0]['href'][/\d+/],
+        watchers: movie.css('td')[2].text,
+        seasons: movie.css('td')[4].text,
+        year: movie.css('td')[5].text
+      }
     end
+    content
   end
 
   def seasons(movie_id)
     seasons_list = {}
-    html = Nokogiri::HTML(URI.open(BASE_URL + "/view/#{movie_id}/"))
+
+    html = parse_html("/view/#{movie_id}/")
     # list of all seasons
-    html.css('.col8 > form > .row[itemprop="season"]').reverse_each do |season|
+    html&.css('.col8 > form > .row[itemprop="season"]').reverse_each do |season|
       season_number = season.css('.flat > a')[0].text
       special_series_counter = 1
 
@@ -75,7 +82,22 @@ class MyShows
     end
     seasons_list
   end
-end
 
-ms = MyShows.new('bad').movies_list
-puts ms
+  # top rated movies from main page
+  def top_rated
+    top_rated_list = {}
+    html = parse_html
+    # list of all movies
+    html&.xpath('/html/body/div[1]/div[4]/div/div')&.[](0..1)&.css('a')&.each do |movie|
+      ru_title = movie.css('.fsHeader').text
+      top_rated_list[:"#{ru_title}"] = {
+        en_title: movie.css('.cFadeLight').text,
+        image_link: movie.search('._img').to_s[/(?<=\().+?(?=\))/],
+        description: movie['href']
+      }
+    end
+    top_rated_list
+  end
+
+end
+puts MyShows.new.top_rated

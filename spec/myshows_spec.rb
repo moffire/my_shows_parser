0c80546e62d0051ee126a6cfb8b1e1d0bfac94e7
moffire <alexeydudarev.spb@gmail.com>
2020-04-18 13:41:33 +0300

    added test for movies_list method

--- spec/myshows_spec.rb
+++ spec/myshows_spec.rb
@@ -4,15 +4,21 @@ require_relative '../my_shows'
 
 
 describe MyShows do
+  fixtures_path = Dir.pwd + '/spec/fixtures/files/main.html'
 
-  fixtures_path = Dir.pwd + '/spec/fixtures/files/'
+  let(:ms_object) { MyShows.new('bad') }
 
   describe '#top_rated' do
-    let(:ms_object) { MyShows.new }
-
-    it 'en_title should be eq Harley Quinn' do
-      stub_const('MyShows::BASE_URL', fixtures_path + 'top_rated.htm')
+    it 'should be eq Harley Quinn' do
+      stub_const('MyShows::BASE_URL', fixtures_path)
       expect(ms_object.top_rated[:movie_title][:en_title]).to eq('Harley Quinn')
+    end
+  end
+
+  describe '#movies_list' do
+    it 'should be eq Breaking Bad' do
+      allow(ms_object).to receive(:parse_html).and_return(Nokogiri::HTML(URI.open(fixtures_path)))
+      expect(ms_object.movies_list[:movie_title][:en_title]).to eq('Breaking Bad')
     end
   end
 
